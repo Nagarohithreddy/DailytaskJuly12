c4f1c6cbd742de43a24de5c17d4bef43cf858174
Nagarohithreddy <142422304+Nagarohithreddy@users.noreply.github.com>
2024-07-12 23:35:51 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,90 @@
+//Datatypes
+//strings
+let a = "Rohith"
+console.log(a,typeof a);
+
+//number
+let age = 18
+console.log(age,typeof age);
+
+//boolean
+let x = true
+console.log(x,typeof x);
+
+//undefined
+let b 
+console.log(b,typeof b);
+
+//null
+let c = null
+console.log(c,typeof c);
+
+//Mathematical Operators
+
+//Arthematic Operators
+let A = 5
+let B = 8
+
+//addition
+
+console.log('Addition',A+B);
+
+//Subtraction
+
+console.log('Subtraction',A-B);
+
+//Multiplication
+
+console.log('Multiplication',A*B);
+
+//Division
+
+console.log('Division',A/B);
+
+//Comparator Operators
+
+let X = 1
+let Y = "1"
+let Z = "2"
+let S = 1
+let R = 2
+
+// == Equality Operator
+console.log(X==Y);
+console.log(X==S);
+//if X is equal to y it returns true otherwise false
+
+// === Strict Equality Operatore
+console.log(X===Y);
+console.log(S===R);
+//Compares if two values are equal and of the same data type.
+
+//!= Not equal operator
+console.log(S!=R);
+console.log(R!=Z);
+//Compares if two values are not equal
+
+//!== Strict Not equal operator
+console.log(S!==Y);
+console.log(R!==S);
+//compares if two values are not equal or of different data types
+
+// Greater than > Operator
+console.log(S>R);
+console.log(R>Y);
+// checks if the left value is greater than the right value
+
+//less tham operator
+console.log(S<R);
+console.log(X<Z);
+//checks  if the left value is less than the right value
+
+//Greater than or equal to >= 
+console.log(R>=S);
+console.log(X>=Z);
+//checks if the left value is greater than or equal to the right value.
+
+//Less than or equal to <= operator
+console.log(X<=Z);
+console.log(S<=R);
+//checks if the left value is less than or equal to the right value.
